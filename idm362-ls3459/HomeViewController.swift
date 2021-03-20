f4bc85ed3dbfb03f3cf9ea28191fcfb58c9dc5d7
Sapphire Sun <suntaiyang2036@gmail.com>
2021-03-20 08:24:26 -0400

    Attach sound files

---
+++ idm362-ls3459/HomeViewController.swift
@@ -0,0 +1,29 @@
+//
+//  HomeViewController.swift
+//  idm362-ls3459
+//
+//  Created by 孙太阳Sapphire on 2021/3/20.
+//
+
+import UIKit
+
+class HomeViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
