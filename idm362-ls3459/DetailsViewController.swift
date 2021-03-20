20a78866fcaa883c190814dae63e63f5d9c69d5e
Sapphire Sun <suntaiyang2036@gmail.com>
2021-03-20 12:28:34 -0400

    Detail view attached

---
+++ idm362-ls3459/DetailsViewController.swift
@@ -0,0 +1,32 @@
+
+
+import UIKit
+
+class DetailsViewController: UIViewController {
+    
+    
+    @IBOutlet weak var displayText: UILabel!
+    @IBOutlet weak var displayImage: UIImageView!
+    
+    var userName:String = "Sentence"
+    var userButtonNum: Int = 0
+    
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do after loading.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
